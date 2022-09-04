1f8c65d0beded72807eef0f197ecd7796ea09624
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-09-04 09:50:51 +0300

    This is the solution of the problem "Create Table Users".

---
+++ T01 Databases Introduction. Data Definition and Datatypes/Exercise/P07 Create Table Users.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `users` (
+`id` INT AUTO_INCREMENT NOT NULL,
+`username` VARCHAR(30) UNIQUE NOT NULL,
+`password` VARCHAR(26) NOT NULL,
+`profile_picture` MEDIUMBLOB,
+`last_login_time` TIMESTAMP,
+`is_deleted` BOOLEAN,
+PRIMARY KEY (`id`));
+
+INSERT INTO `users` (`id`, `username`, `password`, `last_login_time`, `is_deleted`)
+VALUES (1, 'Peter', 'guess', '2020-04-20 10:00', true);
+
+INSERT INTO `users` (`id`, `username`, `password`, `last_login_time`, `is_deleted`)
+VALUES (2, 'George', 'a', '2020-04-20 10:00', true);
+
+INSERT INTO `users` (`id`, `username`, `password`, `last_login_time`, `is_deleted`)
+VALUES (3, 'David', 'b', '2020-04-20 10:00', true);
+
+INSERT INTO `users` (`id`, `username`, `password`, `last_login_time`, `is_deleted`)
+VALUES (4, 'Maria', 'c', '2020-04-20 10:00', true);
+
+INSERT INTO `users` (`id`, `username`, `password`, `last_login_time`, `is_deleted`)
+VALUES (5, 'Sofia', 'd', '2020-04-20 10:00', true);
