1862fdf115a2236c3ac07ad4bce2709e1a667e4b
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-09-07 17:39:52 +0300

    Solution of the problem "Increase Salaries"

---
+++ T02 Basic CRUD/Exercise/P20 Increase Salaries.sql
@@ -0,0 +1,11 @@
+UPDATE `employees`
+SET `salary` = 1.12 * salary
+WHERE `department_id` IN (1, 2, 4, 11);
+SELECT `salary` FROM `employees`;
+
+
+/*
+Engineering, Tool Design, Marketing or Information Services
+*/
+
+
