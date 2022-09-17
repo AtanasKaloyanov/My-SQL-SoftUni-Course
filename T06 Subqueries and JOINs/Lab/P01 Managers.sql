9d3c1ac9bcdc516e1740ae566346a3d304c70db9
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-09-17 17:32:53 +0300

    Final solution of the problem "Manager"

--- T06 Subqueries and JOINs/Lab/P01 Managers.sql
+++ T06 Subqueries and JOINs/Lab/P01 Managers.sql
@@ -1,7 +1,6 @@
-SELECT `E`.`employee_id`, CONCAT(`first_name`, ' ', `last_name`) AS `Full Name`, `D`.`department_id`, `D`.`name`
-FROM `employees` AS `E` 
-JOIN `departments` AS `D`
-ON `D`.`manager_id` = `E`.`employee_id`
-ORDER BY `employee_id`
-LIMIT 5; 
-
+-- the managers – id, full_name, deparment_id and department_name
+SELECT `D`.`manager_id`, CONCAT(`first_name`, ' ', `last_name`) AS `Full Name`, `D`.`department_id`, `D`.`name` 
+FROM `departments` AS `D`
+JOIN `employees` AS `E`
+ON `D`. `manager_id` = `E`.`employee_id`
+ORDER BY `manager_id` LIMIT 5;
