aeb7cc1f295c58645a04eff91af840b04c4ee6ae
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-09-17 15:24:35 +0300

    Final solution of the problem "Trip Organization" with "Nested Select"

--- T05 Table Relations/Lab/P02 Trip Organization.sql
+++ T05 Table Relations/Lab/P02 Trip Organization.sql
@@ -1,9 +1,14 @@
-
-SELECT `driver_id`, `vehicle_type`, 
-(SELECT CONCAT(`first_name`, ' ', `last_name`)
-FROM `campers` AS `C`
-WHERE `C` . `id` = `V` . `driver_id`)
-FROM `vehicles` AS `V` 
+SELECT 
+    `driver_id`,
+    `vehicle_type`,
+    (SELECT 
+            CONCAT(`first_name`, ' ', `last_name`)
+        FROM
+            `campers` AS `C`
+        WHERE
+            `C`.`id` = `V`.`driver_id`) AS `name`
+FROM
+    `vehicles` AS `V`
 
 
 
