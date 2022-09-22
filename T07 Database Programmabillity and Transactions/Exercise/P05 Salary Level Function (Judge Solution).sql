b6ed844061eefe027c4d4166c28a6f8e6cd778e6
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-09-22 17:33:25 +0300

    Judge solution for the problem "Salary Level Function"

---
+++ T07 Database Programmabillity and Transactions/Exercise/P05 Salary Level Function (Judge Solution).sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION `ufn_get_salary_level`(`given_salary` DECIMAL)
+RETURNS VARCHAR(8)
+DETERMINISTIC
+
+BEGIN
+DECLARE `salary_level` VARCHAR(8);
+IF `given_salary` < 30000 THEN SET `salary_level` := 'Low';
+ELSEIF `given_salary` <= 50000 THEN SET `salary_level` := 'Average';
+ELSE SET `salary_level` := 'High';
+END IF;
+RETURN `salary_level`;
+END
