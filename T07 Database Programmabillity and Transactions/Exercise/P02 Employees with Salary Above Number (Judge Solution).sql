957a65f6169d9321587c28b9222e9e8d0f703ecf
AtanasKaloyanov <102095904+AtanasKaloyanov@users.noreply.github.com>
2022-09-22 15:09:38 +0300

    Delete P02 Employees with Salary Above Number (Judge Solution).sql

--- T07 Database Programmabillity and Transactions/Exercise/P02 Employees with Salary Above Number (Judge Solution).sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE `usp_get_employees_salary_above` (`given_number` DECIMAL(12, 4))
-
-BEGIN 
-SELECT `first_name`, `last_name`
-FROM `employees` AS `E`
-WHERE `E`.`salary` >= `given_number`
-ORDER BY `first_name`, `last_name`, `employee_id`;
-END
