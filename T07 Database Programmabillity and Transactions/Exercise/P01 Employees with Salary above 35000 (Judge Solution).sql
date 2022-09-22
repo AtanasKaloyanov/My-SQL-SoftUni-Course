bf51abb86029b3bf44541e5f533c934c5aaa4b58
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-09-22 15:36:45 +0300

    Judge solution for the problem "Employees with salary above 35000"

--- T07 Database Programmabillity and Transactions/Exercise/P01 Employees with Salary above 35000 (Judge Solution).sql
+++ T07 Database Programmabillity and Transactions/Exercise/P01 Employees with Salary above 35000 (Judge Solution).sql
@@ -1,8 +1,10 @@
 CREATE PROCEDURE `usp_get_employees_salary_above_35000`()
 
 BEGIN 
+
 SELECT `first_name`, `last_name`
 FROM `employees`
 WHERE `salary` > 35000
 ORDER BY `first_name`, `last_name`, `employee_id`;
+
 END
