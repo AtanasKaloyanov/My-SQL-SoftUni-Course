1b5322144ce1a2be924cacff56aaaa05e089d65a
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-09-22 15:28:53 +0300

    Judge solution for the problem "Employees with Salary above 35000"

--- T07 Database Programmabillity and Transactions/Exercise/P02 Employees with Salary above Number.sql
+++ T07 Database Programmabillity and Transactions/Exercise/P02 Employees with Salary above Number.sql
@@ -4,10 +4,12 @@ DELIMITER ###
 CREATE PROCEDURE `usp_get_employees_salary_above` (`given_number` DECIMAL(12, 4))
 
 BEGIN 
+
 SELECT `first_name`, `last_name`
 FROM `employees` AS `E`
 WHERE `E`.`salary` >= `given_number`
 ORDER BY `first_name`, `last_name`, `employee_id`;
+
 END
 ###
 
