9394913998592cc2da0fe63c6994a52f8efdfaa9
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-09-23 17:15:34 +0300

    Second solution for the problem "Employees by Salary"

--- T07 Database Programmabillity and Transactions/Exercise/P06 Employees by Salary (Second solution).sql
+++ T07 Database Programmabillity and Transactions/Exercise/P06 Employees by Salary (Second solution).sql
@@ -4,7 +4,6 @@ and print the names of all employees that have given level of salary.
 The result should be sorted by first_name then by last_name both in descending order
 */
 
-
 DROP FUNCTION `ufn_get_salary_level`;
 DROP PROCEDURE `usp_get_employees_by_salary_level`;
 
@@ -22,7 +21,6 @@ END IF;
 RETURN `salary_level`;
 END;
 
-
 CREATE PROCEDURE `usp_get_employees_by_salary_level`(`salary_level` VARCHAR(8))
 
 BEGIN
@@ -38,28 +36,3 @@ DELIMITER ;
 -- CALL `usp_get_employees_by_salary_level`('Average');
 CALL `usp_get_employees_by_salary_level`('High');
 
-/*
-DELIMITER %%
-CREATE FUNCTION ufn_get_salary_level(salary DECIMAL(19, 4))
-RETURNS VARCHAR(8)
-DETERMINISTIC
-BEGIN
-	DECLARE salary_level VARCHAR(8);
-    IF salary < 30000 THEN SET salary_level := 'Low';
-    ELSEIF salary <= 50000 THEN SET salary_level := 'Average';
-	ELSE SET salary_level := 'High';
-    END IF;
-    RETURN salary_level;
-END;
-
-
-CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(8)) 
-BEGIN
-	SELECT first_name, last_name
-	FROM employees
-    WHERE ufn_get_salary_level(salary) LIKE salary_level
-    ORDER BY first_name DESC, last_name DESC;
-END;
-
-CALL `usp_get_employees_by_salary_level`('Low')
-*/
