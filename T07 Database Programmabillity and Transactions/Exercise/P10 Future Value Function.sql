869adff2b1a2316f8d01b732f76c1c6190669a10
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-10-01 09:56:55 +0300

    Solution for the problem "Future Value Function"

---
+++ T07 Database Programmabillity and Transactions/Exercise/P10 Future Value Function.sql
@@ -0,0 +1,18 @@
+DROP FUNCTION `ufn_calculate_future_value`;
+
+DELIMITER ###
+CREATE FUNCTION `ufn_calculate_future_value`(`sum` DOUBLE(19, 4), `yearly_interest_rate` DOUBLE, `number_of_years` INT)
+RETURNS DECIMAL(19, 4)
+DETERMINISTIC
+
+BEGIN 
+DECLARE `final_sum` DOUBLE(19,4);
+SET `final_sum` = `sum` * POW((1 + `yearly_interest_rate`), `number_of_years`);
+RETURN `final_sum`;
+END
+###
+
+DELIMITER ;
+
+SELECT `ufn_calculate_future_value`(1000, 0.5, 5);
+
