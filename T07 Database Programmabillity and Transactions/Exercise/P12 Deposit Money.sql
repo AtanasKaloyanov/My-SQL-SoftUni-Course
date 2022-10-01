3685157ee0811350c9ff2e3a809baf1f69b53b23
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-10-01 11:54:52 +0300

    Solution for the problem "Deposit Money"

---
+++ T07 Database Programmabillity and Transactions/Exercise/P12 Deposit Money.sql
@@ -0,0 +1,31 @@
+/*
+Add stored procedures usp_withdraw_money(account_id, money_amount) that operate in transactions. 
+Make sure to guarantee withdraw is done only when balance is enough and money_amount is valid positive number. 
+Work with precision up to fourth sign after decimal point. 
+The procedure should produce exact results working with the specified precision.
+*/
+SET SQL_SAFE_UPDATES = 0;
+
+DROP PROCEDURE `usp_deposit_money`;
+DELIMITER ###
+CREATE PROCEDURE `usp_deposit_money`(`account_id` INT, `money_amount` DECIMAL(19,4))
+
+BEGIN 
+
+START TRANSACTION;
+
+IF (`money_amount` <= 0) THEN ROLLBACK;
+ELSE 
+UPDATE `accounts`
+SET `balance` = `balance` + `money_amount`;
+SELECT `A`.`id`, `account_holder_id`, `balance`
+FROM `accounts` AS `A`
+WHERE `A`.`id` = `account_id`;
+
+COMMIT;
+END IF;
+
+END
+###
+
+CALL `usp_deposit_money`(1, 10);
