bad6eecf2b021d7a66a74d4890d775341524c488
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-10-06 18:44:41 +0300

    Solution for the problem "Insert"

---
+++ Exam Preparation/Exam2 13.02.2022/P02 Insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO `reviews`(`content`, `rating`, `picture_url`, `published_at`)
+SELECT
+LEFT(`description`, 15),
+`price` / 8,
+REVERSE(`name`), 
+'2010-10-10'
+FROM `products`
+WHERE `id` >= 5
